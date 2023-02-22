d87dcba4164be53e822a21e396ed385f114ef21f
SheenaTaurus <lameseferreira@gmail.com>
2023-02-21 22:54:26 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * * main - check the code
- *  *
- *   *Return: Always 0.
- *    */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
     int r;
@@ -12,7 +12,7 @@ int main(void)
     print_last_digit(98);
     print_last_digit(0);
     r = print_last_digit(-1024);
-    _putchar('4' + r);
+    _putchar('0' + r);
     _putchar('\n');
     return (0);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,16 +2,19 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- *@n: the number
- * Return: the value of the last digit
+ *@r: numbers last digit result
+ * Return: value of the last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int r)
 {
-	int ld = n % 10;
+	int n;
 
-	if (ld < 0)
-		ld *= -1;
-	_putchar(ld + '0');
-
-	return (0);
+	if (r < 0)
+	{
+		n = -1 * (r % 10);
+	}
+	else
+		n = r % 10;
+	_putchar(( n % 10) + '0');
+	return (n % 10);
 }
