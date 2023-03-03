af36519bf963c9fecbe538ca3ef65f3161e56e4e
SheenaTaurus <lameseferreira@gmail.com>
2023-03-03 11:44:19 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,6 @@
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized
- *
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
@@ -13,9 +12,8 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == '~' ||
+		index++;
+		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
 			str[index - 1] == ',' ||
@@ -30,7 +28,6 @@ char *cap_string(char *str)
 			str[index - 1] == '}' ||
 			index == 0)
 			str[index] -= 32;
-
 		index++;
 	}
 	return (str);
