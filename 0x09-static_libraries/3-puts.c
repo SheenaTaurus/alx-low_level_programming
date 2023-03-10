8fc0637e710a25645f6995f8c7b942c035e35c56
SheenaTaurus <lameseferreira@gmail.com>
2023-03-10 14:21:37 -0800

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program which prints a line including puts function
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
